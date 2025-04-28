33ff8c2b655bd074c179a94a1dc5e8ada2eaffca
Yana <syanaa9@gmail.com>
2025-04-28 21:55:35 +1000

    feat: implement delete()

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import  { rm } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
     // Write your code here 
+    try {
+        const fileURL = new URL('./files/fileToRemove.txt', import.meta.url);
+        const filePath = fileURLToPath(fileURL);
+        await rm(filePath);     
+        
+    } catch (error) {
+        if(error.code === 'ENOENT'){
+            throw new Error('FS operation failed');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await remove();
