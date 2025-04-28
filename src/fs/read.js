6710fa6c8aee924cb9822564b66a6dcf89a1ff5c
Yana <syanaa9@gmail.com>
2025-04-28 22:17:18 +1000

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'node:url';
+import { readFile } from "node:fs/promises";
+
+
 const read = async () => {
     // Write your code here 
+    try {
+
+        const fileURL = new URL('./files/fileToRead.txt', import.meta.url);
+        const filePath = fileURLToPath(fileURL);
+        const contents = await readFile(filePath, { encoding: 'utf8' });
+        console.log(contents);
+        
+    } catch (error) {
+        if(error.code === 'ENOENT'){
+            throw new Error('FS operation failed');
+        } else {
+            throw error;
+        } 
+    }
 };
 
 await read();
