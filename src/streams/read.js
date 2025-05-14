f082ab1a676e5b53479e86906a181ce349e9b29b
Yana <syanaa9@gmail.com>
2025-05-14 16:45:10 +1000

    implement read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fileURL = new URL('./files/fileToRead.txt', import.meta.url);
+        const filePath = fileURLToPath(fileURL);
+
+
+        /* const stream = createReadStream(filePath);
+        stream.on('data', (chunk) => process.stdout.write(chunk));
+        stream.on('end', () => console.log('\nDone')); */
+
+        const stream  = createReadStream(filePath); //create readable stream
+        stream.pipe(process.stdout); //redirect stream in standart out(like console.log)
+        
+        stream.on('error', (err) => {
+            if (err.code === "ENOENT") {
+                throw new Error('FS operation failed');
+            } else {
+                throw err;
+            }
+        });
+    } catch (error) {
+        
+    }
+
 };
 
 await read();
