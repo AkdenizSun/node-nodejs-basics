61225df56f7d63f409f3ece54e4d1d2a2fc043ff
Yana <syanaa9@gmail.com>
2025-05-15 14:25:34 +1000

    feat: implement write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,36 @@
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+
 const write = async () => {
     // Write your code here 
+    try {
+        const fileURL = new URL('./files/fileToWrite.txt', import.meta.url);
+        const filePath = fileURLToPath(fileURL);
+
+        const writableStream = createWriteStream(filePath);
+        
+        process.stdin.pipe(writableStream);
+
+        // writableStream.on('error', (err) => {
+        //     throw new Error('FS operation failed');
+        // });
+
+        process.stdin.on('data', (chunk) => {
+            writableStream.write(chunk);
+        });
+
+        process.stdin.on('end', () =>{
+            writableStream.end();
+        })
+
+
+
+
+    } catch (error) {
+        
+    }
 };
 
 await write();
